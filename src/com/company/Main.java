66359b415330534f8d347805f9f671fb37f73cc1
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-28 16:17:38 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        String s = "buckyrobertsbuckyroberts";
+
+        System.out.println(s.indexOf("rob"));
+
+         */
+        String a = "bacon";
+        String b = "   monster   ";
+
+        System.out.println(a.concat(b));
+        System.out.println(a.replace("b","f"));
+        System.out.println(b.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(b.trim());
+
+    }
+}
